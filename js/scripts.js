43074899f1da4a3766367e7f23f75d604d5d4d66
dchardin <dch84121@gmail.com>
2017-08-27 17:46:52 -0400

    a few minor cosmetic changes, as well as some recalculations in the script to produce a larger image.

--- js/scripts.js
+++ js/scripts.js
@@ -10,8 +10,8 @@ function grow(el) {
 var generate = function() {
 	var text = document.getElementById('text').value.split("\n").join("\n");
 	var x = 12.5;
-	var y = 15;
-	var lineheight = 30;
+	var y = 40;
+	var lineheight = 70;
 	var lines = text.split('\n');
 	var lineLengthOrder = lines.slice(0).sort(function(a, b) {
 		return b.length - a.length;
@@ -22,8 +22,8 @@ var generate = function() {
 	ctx.fillStyle = "#232323";
 	ctx.fillRect(0, 0, canvas.width, canvas.height);
 	ctx.textBaseline = "middle";
-	ctx.font="20px Anonymous Pro";
-	ctx.fillStyle = "#BBBBBB";
+	ctx.font="70px Anonymous Pro";
+	ctx.fillStyle = "#39FF14";
 	for (var i = 0; i<lines.length; i++)
 		ctx.fillText(lines[i], x, y + (i*lineheight) );
 	img.src = ctx.canvas.toDataURL();
